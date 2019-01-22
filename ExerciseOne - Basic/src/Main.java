467a94dde00f89f6fe47383764b32a7f5549ddae
JohnTorrance8270 <johnabertay8270@hotmail.com>
2019-01-22 09:38:20 +0000

    Initial commit for this repo, Completed Calc project and the start of the QA Java excercises

---
+++ ExerciseOne - Basic/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		String outputMessage = "Hello World!";
+		printStringMessage(outputMessage);
+	}
+	
+	private static void printStringMessage(String message)
+	{
+		System.out.println(message);
+	}
+}
